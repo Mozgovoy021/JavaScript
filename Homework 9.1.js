2b4c80f33beec4c1938d5e6f5feede240f9debe8
Oleksandr Mozghovyi <61834094+Mozgovoy021@users.noreply.github.com>
2024-08-08 15:12:19 +0300

    Add files via upload

---
+++ Homework 9.1.js
@@ -0,0 +1,65 @@
+function findValueByKey(companyName, companyObject) {
+    if (companyObject.name === companyName) return companyObject;
+
+    if (companyObject.clients) {
+        for (let client of companyObject.clients) {
+            let result = findValueByKey(companyName, client);
+            if (result) return result;
+        }
+    }
+
+    if (companyObject.partners) {
+        for (let partner of companyObject.partners) {
+            let result = findValueByKey(companyName, partner);
+            if (result) return result;
+        }
+    }
+
+    return null;
+}
+
+const company = {
+    name: 'Велика Компанія',
+    type: 'Головна компанія',
+    platform: 'Платформа для продажу квитків',
+    sellsSolution: 'Рішення для продажу квитків',
+    clients: [
+        {
+            name: 'Клієнт 1',
+            type: 'subCompany',
+            uses: 'ПО для продажу квитків',
+            sells: 'Рішення для продажу квитків',
+            partners: [
+                {
+                    name: 'Клієнт 1.1',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                },
+                {
+                    name: 'Клієнт 1.2',
+                    type: 'subSubCompany',
+                    uses: 'Рішення для продажу квитків',
+                    sells: 'Рішення для продажу квитків',
+                    partners: [
+                        {
+                            name: 'Клієнт 1.2.3',
+                            type: 'subSubCompany',
+                            uses: 'Рішення для продажу квитків',
+                            sells: 'Рішення для продажу квитків',
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'Клієнт 2',
+            type: 'subCompany',
+            uses: 'ПО для продажу квитків',
+            sells: 'Рішення для продажу квитків'
+        }
+    ]
+};
+
+const result = findValueByKey('Клієнт 1', company);
+console.log(result);
